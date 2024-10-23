9371981474183ce569fd6785cf607f26671145a2
Dauvnir <pdawidpolak@gmail.com>
2024-10-23 15:38:29 +0200

    Disabled minifyin build.

--- SuperMarioDevs/vite.config.js
+++ SuperMarioDevs/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		minify: false, // Disable minification to check if that's causing the issue
-	},
 });
